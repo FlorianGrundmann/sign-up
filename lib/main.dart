84871c807263a11b2310f92e7f544b77a7f77d92
Florian G <florian.grundmann@freenet.de>
2020-07-03 16:30:32 +0700

    Added dependency injection with injected.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
 
+import 'injection.dart';
 import 'presentation/pages/email_login_page.dart';
 import 'presentation/styles/white_theme.dart';
 
 void main() {
+  configureInjection(Environment.prod);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Authentication Demo',
       theme: WhiteTheme().themeData,
       home: const EmailLoginPage(),
     );
